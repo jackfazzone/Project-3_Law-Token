bb0ae43fac953a8270d125cbf798313910b2f315
Jack Fazzone <anthony.j.fazzone@gmail.com>
2021-03-08 19:11:32 -0600

    contract updates

--- Jack/Copy of Law Token.sol
+++ Jack/Copy of Law Token.sol
@@ -19,6 +19,7 @@ contract LawToken is ERC721Full {
 
     struct CivilCase {
       //Implement CivilCases struct
+      address payable plaintiff;
       string caseArea;
       string caseDescription;
       string defendant;
@@ -32,29 +33,27 @@ contract LawToken is ERC721Full {
       uint estimatedSettlement;
       uint fundingDeadline;
     }
+    
+    struct LawFirm {
+    address payable lawFirm;
+    string firmName;
+    string practceArea;
+    string state;
+    string city;
+    string message;
+    
+    }
 
     // Stores tokenCounter => CivilCase
     // Only permanent data that you would need to use in a smart contract later should be stored on-chain
     mapping(uint => CivilCase) public CivilCases;
+    mapping(uint => LawFirm) public firms;
 
+    event caseAssigned(uint tokenID, string reportURI);
     event caseSentenced(uint tokenId, string reportURI);
   
-  
-// I just put in the function below in case we want to list the cases before a law firm is assigned so that firms can bid on rate, the client can select a firm, etc.
-// If we go in that direction, where should we store this list? Seems like it does not make sense to mint erc721s for a listed case and then turn around and mint another for the registry
 // What if we list and mint with empty values on the attorney/ firm params and then update them after assignment with an "Attorney Assigned" emission?
 
-    // function listCivilCase(address payable caseOwner,
-    //     string caseType,
-    //     string caseDescription,
-    //     string eventLocation,
-    //     string eventDate,
-    //     string plaintiffInjury,
-    //     string defendant) public returns (uint) {
-    //     listCounter.increment();
-    //     uint caseId = listCounter.current();
-    //     //assignmentDeadline = //maybe we want to hardcode a value here
-    //     }
 
     function registerCivilCase(address payable caseOwner,
         string memory caseArea,
@@ -64,31 +63,54 @@ contract LawToken is ERC721Full {
         string memory plaintiffInjury,
         string memory defendant,
         uint totalSuitExpenses,             // given by assigned attorney
-        string memory lawFirm,
+        string memory firmName,
+       // address payable lawFirm,
         string memory attorney,             
         uint fundingAmount,                 // up-front cash bid of winning firm (crowdsale goal)
         uint firmEquity,                    // equity bid of winning (out of 100), will be no. of erc20s to distribute after funding
         //uint estimatedSettlement,         // 100*(totalSuitExpenses - fundingAmount)/firmEquity ... maybe instead of graduated incentive fee structure?
-        //uint estimatedCoinValue           // ^^ /100
+        //uint estimatedCoinValue,          // ^^ /100
         uint fundingDeadline,
         string memory estimatedRangeSettlement,
         uint setllementPercentageSplit,
         string memory attorneyIncentiveFeeStructure, 
         
         string memory caseURI) public returns(uint) {
-      //Implement registerCivilCase
-      caseCounter.increment();
-      uint caseId = caseCounter.current();
+        //Implement registerCivilCase
+        caseCounter.increment();
+        uint caseId = caseCounter.current();
       
-      fundingDeadline = now + 30 days;
+        fundingDeadline = now + 30 days;
       
-      _mint(caseOwner, caseId,0);
-      _setTokenURI(caseId, caseURI);
+        _mint(caseOwner, caseId, 0);
+        _setTokenURI(caseId, caseURI);
       
-      CivilCases[caseId] = CivilCase(caseArea, caseDescription, defendant, "No firm assigned.", 0,0,0,0,0,0, fundingDeadline);
+        CivilCases[caseId] = CivilCase(caseOwner, caseArea, caseDescription, defendant, "No firm assigned.", 0,0,0,0,0,0, fundingDeadline);
 
         return caseId;
         }
+        
+     function registerLawFirm(
+        string memory firmName,
+        string memory practceArea,
+        string memory state,
+        string memory city,
+        string memory message,
+        
+        string memory firmURI) public returns(uint) {
+        
+        firmCounter.increment();
+        uint firmID = firmCounter.current();
+        
+        _mint(msg.sender, firmID);
+        _setTokenURI(firmID, firmURI);
+        
+        firms[firmID] = LawFirm(msg.sender,firmName, practceArea, state, city, message, 0);
+        
+        return firmID;
+        }
+        
+        
     function fundingcase(address caseOwner, uint amount, address investor ) public{}
     
     function cancelCivilCasending(address investor) public view returns (uint) {
